4709274b7bef39051e6203bfd6c3b82d58648afc
saloed <napster_1997@mail.ru>
2022-06-12 02:33:29 +0300

    KContext and initial Z3 support ( #2, #3, #5 )
    toString for everything
    
    array const
    
    remove warnings suppression
    
    minimize annoying ensureContextMatch calls
    
    drop KDecl.apply KContext extension receiver
    
    Use comparison by reference for KDecl equals
    
    internal constructor for decls and sorts
    
    KAst base class with context for all ksmt entities
    
    explicit type specification in public api
    
    remove args from App to avoid duplicate argument storage
    
    remove equalTo
    
    rewrite interning: use separate caches for each expression type
    
    move all builders into context
    
    change decl application return type to App
    
    add ite expr
    
    decl visitor
    
    sort visitor
    
    add clarification about mkApp
    
    loose too many functions constraint
    
    setup detekt linter with default config
    
    transformers
    
    lazy instantiation of expression decl
    
    int & real theories
    
    add quantifiers
    
    Use KFuncDecl.apply to fix possible KApp duplication
    
    use KApp as base for all applications
    
    drop KSort type parameter
    
    use more simple types
    
    expression interning draft
    
    initial type hierarchy

---
+++ src/main/kotlin/org/ksmt/decl/Bool.kt
@@ -0,0 +1,49 @@
+package org.ksmt.decl
+
+import org.ksmt.KContext
+import org.ksmt.expr.KExpr
+import org.ksmt.expr.KApp
+import org.ksmt.sort.KBoolSort
+import org.ksmt.sort.KSort
+
+class KAndDecl internal constructor(ctx: KContext) :
+    KFuncDeclChain<KBoolSort, KBoolSort>(ctx, "and", ctx.mkBoolSort(), ctx.mkBoolSort()) {
+    override fun KContext.applyChain(args: List<KExpr<KBoolSort>>): KApp<KBoolSort, KExpr<KBoolSort>> = mkAnd(args)
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KOrDecl internal constructor(ctx: KContext) :
+    KFuncDeclChain<KBoolSort, KBoolSort>(ctx, "or", ctx.mkBoolSort(), ctx.mkBoolSort()) {
+    override fun KContext.applyChain(args: List<KExpr<KBoolSort>>): KApp<KBoolSort, KExpr<KBoolSort>> = mkOr(args)
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KNotDecl internal constructor(ctx: KContext) :
+    KFuncDecl1<KBoolSort, KBoolSort>(ctx, "not", ctx.mkBoolSort(), ctx.mkBoolSort()) {
+    override fun KContext.apply(arg: KExpr<KBoolSort>): KApp<KBoolSort, KExpr<KBoolSort>> = mkNot(arg)
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KEqDecl<T : KSort> internal constructor(ctx: KContext, argSort: T) :
+    KFuncDecl2<KBoolSort, T, T>(ctx, "eq", ctx.mkBoolSort(), argSort, argSort) {
+    override fun KContext.apply(arg0: KExpr<T>, arg1: KExpr<T>): KApp<KBoolSort, *> = mkEq(arg0, arg1)
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KIteDecl<T : KSort> internal constructor(ctx: KContext, argSort: T) :
+    KFuncDecl3<T, KBoolSort, T, T>(ctx, "ite", argSort, ctx.mkBoolSort(), argSort, argSort) {
+    override fun KContext.apply(arg0: KExpr<KBoolSort>, arg1: KExpr<T>, arg2: KExpr<T>): KApp<T, *> =
+        mkIte(arg0, arg1, arg2)
+
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KTrueDecl internal constructor(ctx: KContext) : KConstDecl<KBoolSort>(ctx, "true", ctx.mkBoolSort()) {
+    override fun apply(args: List<KExpr<*>>): KApp<KBoolSort, *> = ctx.mkTrue()
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
+
+class KFalseDecl internal constructor(ctx: KContext) : KConstDecl<KBoolSort>(ctx, "false", ctx.mkBoolSort()) {
+    override fun apply(args: List<KExpr<*>>): KApp<KBoolSort, *> = ctx.mkFalse()
+    override fun <R> accept(visitor: KDeclVisitor<R>): R = visitor.visit(this)
+}
