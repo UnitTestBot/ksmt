6e9345014a9a566a99d15b8fa068b70c7bb21e32
Saloed <napster_1997@mail.ru>
2022-06-20 18:15:01 +0300

    sort visitor

--- src/main/kotlin/org/ksmt/sort/KSort.kt
+++ src/main/kotlin/org/ksmt/sort/KSort.kt
@@ -2,16 +2,28 @@ package org.ksmt.sort
 
 import org.ksmt.expr.KBVSize
 
-interface KSort
+interface KSort {
+    fun <T> accept(visitor: KSortVisitor<T>): T
+}
 
-object KBoolSort : KSort
+object KBoolSort : KSort {
+    override fun <T> accept(visitor: KSortVisitor<T>): T = visitor.visit(this)
+}
 
-interface KArithSort<T: KArithSort<T>> : KSort
+interface KArithSort<T : KArithSort<T>> : KSort
 
-object KIntSort: KArithSort<KIntSort>
+object KIntSort : KArithSort<KIntSort> {
+    override fun <T> accept(visitor: KSortVisitor<T>): T = visitor.visit(this)
+}
 
-object KRealSort: KArithSort<KRealSort>
+object KRealSort : KArithSort<KRealSort> {
+    override fun <T> accept(visitor: KSortVisitor<T>): T = visitor.visit(this)
+}
 
-class KBVSort<S : KBVSize> : KSort
+class KBVSort<S : KBVSize> : KSort {
+    override fun <T> accept(visitor: KSortVisitor<T>): T = visitor.visit(this)
+}
 
-class KArraySort<D : KSort, R : KSort>(val domain: D, val range: R) : KSort
+class KArraySort<D : KSort, R : KSort>(val domain: D, val range: R) : KSort {
+    override fun <T> accept(visitor: KSortVisitor<T>): T = visitor.visit(this)
+}
