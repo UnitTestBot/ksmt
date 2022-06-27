8893c8f4ba8192665884e5130d9abc433079ca39
Saloed <napster_1997@mail.ru>
2022-06-27 14:06:52 +0300

    solver api (KSolver, KModel)

---
+++ src/main/kotlin/org/ksmt/solver/KModel.kt
@@ -0,0 +1,23 @@
+package org.ksmt.solver
+
+import org.ksmt.decl.KDecl
+import org.ksmt.expr.KExpr
+import org.ksmt.sort.KSort
+
+interface KModel {
+    val declarations: Set<KDecl<*>>
+    fun <T : KSort> eval(expr: KExpr<T>, complete: Boolean = false): KExpr<T>
+    fun <T : KSort> interpretation(decl: KDecl<T>): KFuncInterp<T>?
+    fun detach(): KModel
+
+    data class KFuncInterp<T : KSort>(
+        val vars: List<KDecl<*>>,
+        val entries: List<KFuncInterpEntry<T>>,
+        val default: KExpr<T>
+    )
+
+    data class KFuncInterpEntry<T : KSort>(
+        val args: List<KExpr<*>>,
+        val value: KExpr<T>
+    )
+}
