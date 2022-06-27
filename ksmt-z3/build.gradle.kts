f5e6da653901d5acb0c002847098125b1eecb593
Saloed <napster_1997@mail.ru>
2022-06-27 14:23:48 +0300

    init ksmt-z3 module

---
+++ ksmt-z3/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.gradle.language.jvm.tasks.ProcessResources
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
+}
+
+repositories {
+    mavenCentral()
+    flatDir { dirs("dist") }
+}
+
+val z3native by configurations.creating
+
+dependencies {
+    implementation(project(":ksmt-core"))
+    implementation("org.sosy-lab", "javasmt-solver-z3", "4.8.9-sosy1")
+    testImplementation(kotlin("test"))
+    z3native("com.microsoft.z3", "z3-native-win64", "4.8.9.1", ext = "zip")
+    z3native("com.microsoft.z3", "z3-native-linux64", "4.8.9.1", ext = "zip")
+    z3native("com.microsoft.z3", "z3-native-osx", "4.8.9.1", ext = "zip")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config = files(rootDir.resolve("detekt.yml"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<ProcessResources> {
+    z3native.resolvedConfiguration.resolvedArtifacts.forEach { artifact ->
+        from(zipTree(artifact.file)) {
+            into("lib/x64")
+        }
+    }
+}
