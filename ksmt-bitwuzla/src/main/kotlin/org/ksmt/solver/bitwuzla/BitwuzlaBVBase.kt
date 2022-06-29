e4da8e120247f2fa72d3f65a70db62a2e044866e
Saloed <napster_1997@mail.ru>
2022-06-29 17:19:53 +0300

    bitwuzla JNA bindings

---
+++ ksmt-bitwuzla/src/main/kotlin/org/ksmt/solver/bitwuzla/BitwuzlaBVBase.kt
@@ -0,0 +1,22 @@
+package org.ksmt.solver.bitwuzla
+
+/**
+ * The base for strings representing bit-vector values.<br></br>
+ * enum values<br></br>
+ * *native declaration : bitwuzla.h:26*
+ */
+enum class BitwuzlaBVBase(val value: Int) {
+    /** < binary  */
+    BITWUZLA_BV_BASE_BIN(0),
+
+    /** < decimal  */
+    BITWUZLA_BV_BASE_DEC(1),
+
+    /** < hexadecimal  */
+    BITWUZLA_BV_BASE_HEX(2);
+
+    companion object {
+        private val valueMapping = BitwuzlaBVBase.values().associateBy { it.value }
+        fun fromValue(value: Int): BitwuzlaBVBase = valueMapping.getValue(value)
+    }
+}
