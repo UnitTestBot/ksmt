7dae232d43898e383618cfcad9e48d21fadfcaec
Saloed <napster_1997@mail.ru>
2022-07-11 11:30:05 +0300

    fix BV model completion

--- ksmt-core/src/main/kotlin/org/ksmt/solver/model/KModelEvaluator.kt
+++ ksmt-core/src/main/kotlin/org/ksmt/solver/model/KModelEvaluator.kt
@@ -160,7 +160,7 @@ open class KModelEvaluator(
             override fun visit(sort: KBoolSort): KExpr<T> = trueExpr as KExpr<T>
             override fun visit(sort: KIntSort): KExpr<T> = 0.intExpr as KExpr<T>
             override fun visit(sort: KRealSort): KExpr<T> = mkRealNum(0) as KExpr<T>
-            override fun <S : KBVSort> visit(sort: S): KExpr<T> = mkBV(0) as KExpr<T>
+            override fun <S : KBVSort> visit(sort: S): KExpr<T> = mkBV("0", sort.sizeBits) as KExpr<T>
             override fun <D : KSort, R : KSort> visit(sort: KArraySort<D, R>): KExpr<T> =
                 mkArrayConst(sort, sort.range.sampleValue()) as KExpr<T>
         })
