200d9e4ff0602f9a4ca36ae39a7992e4f96a4f32
Saloed <napster_1997@mail.ru>
2022-07-12 14:10:23 +0300

    solver incremental api

--- ksmt-z3/src/main/kotlin/org/ksmt/solver/z3/KZ3Solver.kt
+++ ksmt-z3/src/main/kotlin/org/ksmt/solver/z3/KZ3Solver.kt
@@ -2,6 +2,7 @@ package org.ksmt.solver.z3
 
 import com.microsoft.z3.BoolExpr
 import com.microsoft.z3.Context
+import com.microsoft.z3.Solver
 import com.microsoft.z3.Status
 import com.microsoft.z3.Z3Exception
 import org.ksmt.KContext
@@ -12,12 +13,16 @@ import org.ksmt.solver.KSolverException
 import org.ksmt.solver.KSolverStatus
 import org.ksmt.sort.KBoolSort
 import org.ksmt.utils.NativeLibraryLoader
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
 
+@Suppress("TooManyFunctions")
 open class KZ3Solver(private val ctx: KContext) : KSolver {
     private val z3Ctx = Context()
     private val solver = z3Ctx.mkSolver()
     private val z3InternCtx = KZ3InternalizationContext()
     private var lastCheckStatus = KSolverStatus.UNKNOWN
+    private var currentScope: UInt = 0u
     private val sortInternalizer by lazy {
         createSortInternalizer(z3InternCtx, z3Ctx)
     }
@@ -54,35 +59,95 @@ open class KZ3Solver(private val ctx: KContext) : KSolver {
         z3Ctx: Context
     ) = KZ3ExprConverter(ctx, internCtx)
 
-    override fun assert(expr: KExpr<KBoolSort>) = try {
+    override fun push() {
+        solver.push()
+        currentScope++
+    }
+
+    override fun pop(n: UInt) {
+        require(n <= currentScope) {
+            "Can not pop $n scope levels because current scope level is $currentScope"
+        }
+        if (n == 0u) return
+        solver.pop(n.toInt())
+        currentScope -= n
+    }
+
+    override fun assert(expr: KExpr<KBoolSort>) = z3Try {
         val z3Expr = with(exprInternalizer) { expr.internalize() }
         solver.add(z3Expr as BoolExpr)
-    } catch (ex: Z3Exception) {
-        throw KSolverException(ex)
     }
 
-    override fun check(): KSolverStatus = try {
-        val status = solver.check() ?: Status.UNKNOWN
-        when (status) {
-            Status.SATISFIABLE -> KSolverStatus.SAT
-            Status.UNSATISFIABLE -> KSolverStatus.UNSAT
-            Status.UNKNOWN -> KSolverStatus.UNKNOWN
-        }.also { lastCheckStatus = it }
-    } catch (ex: Z3Exception) {
-        throw KSolverException(ex)
+    override fun assertAndTrack(expr: KExpr<KBoolSort>): KExpr<KBoolSort> = z3Try {
+        val z3Expr = with(exprInternalizer) { expr.internalize() } as BoolExpr
+
+        @Suppress("ForbiddenComment") // todo: use our fresh const when available
+        val trackVar = z3Ctx.mkFreshConst("track", z3Ctx.boolSort) as BoolExpr
+
+        solver.assertAndTrack(z3Expr, trackVar)
+        with(exprConverter) { trackVar.convert() }
     }
 
-    override fun model(): KModel = try {
+    override fun check(timeout: Duration): KSolverStatus = z3Try {
+        solver.updateTimeout(timeout)
+        solver.check().processCheckResult()
+    }
+
+    @Suppress("SpreadOperator")
+    override fun checkWithAssumptions(
+        assumptions: List<KExpr<KBoolSort>>,
+        timeout: Duration
+    ): KSolverStatus = z3Try {
+        val z3Assumptions = with(exprInternalizer) { assumptions.map { it.internalize() as BoolExpr } }
+        solver.updateTimeout(timeout)
+        solver.check(*z3Assumptions.toTypedArray()).processCheckResult()
+    }
+
+    override fun model(): KModel = z3Try {
         require(lastCheckStatus == KSolverStatus.SAT) { "Model are only available after SAT checks" }
         val model = solver.model
         KZ3Model(model, ctx, z3InternCtx, exprInternalizer, z3Ctx, exprConverter)
-    } catch (ex: Z3Exception) {
-        throw KSolverException(ex)
+    }
+
+    override fun unsatCore(): List<KExpr<KBoolSort>> = z3Try {
+        require(lastCheckStatus == KSolverStatus.UNSAT) { "Unsat cores are only available after UNSAT checks" }
+        val z3Core = solver.unsatCore
+        with(exprConverter) { z3Core.map { it.convert() } }
+    }
+
+    override fun reasonOfUnknown(): String = z3Try {
+        require(lastCheckStatus == KSolverStatus.UNKNOWN) { "Unknown reason is only available after UNKNOWN checks" }
+        solver.reasonUnknown
     }
 
     override fun close() {
         z3InternCtx.close()
         z3Ctx.close()
+    }
+
+    private fun Status?.processCheckResult() = when (this) {
+        Status.SATISFIABLE -> KSolverStatus.SAT
+        Status.UNSATISFIABLE -> KSolverStatus.UNSAT
+        Status.UNKNOWN -> KSolverStatus.UNKNOWN
+        null -> KSolverStatus.UNKNOWN
+    }.also { lastCheckStatus = it }
+
+    private fun Solver.updateTimeout(timeout: Duration) {
+        val z3Timeout = if (timeout == Duration.INFINITE) {
+            UInt.MAX_VALUE.toInt()
+        } else {
+            timeout.toInt(DurationUnit.MILLISECONDS)
+        }
+        val params = z3Ctx.mkParams().apply {
+            add("timeout", z3Timeout)
+        }
+        setParameters(params)
+    }
+
+    private inline fun <reified T> z3Try(body: () -> T): T = try {
+        body()
+    } catch (ex: Z3Exception) {
+        throw KSolverException(ex)
     }
 
     companion object {
