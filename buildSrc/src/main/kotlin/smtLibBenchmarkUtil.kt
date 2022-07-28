c65c48bf610f3cb45e506972dd17119102281a84
Saloed <napster_1997@mail.ru>
2022-07-28 21:32:44 +0300

    don't download smtlib benchmarks which are already exists

--- buildSrc/src/main/kotlin/smtLibBenchmarkUtil.kt
+++ buildSrc/src/main/kotlin/smtLibBenchmarkUtil.kt
@@ -12,14 +12,18 @@ fun Project.mkSmtLibBenchmarkTestData(name: String) = tasks.register("smtLibBenc
         val downloadTarget = path.resolve("$name.zip")
         val repoUrl = "https://clc-gitlab.cs.uiowa.edu:2443"
         val url = "$repoUrl/api/v4/projects/SMT-LIB-benchmarks%2F$name/repository/archive.zip"
-        download().run {
-            src(url)
-            dest(downloadTarget)
-            overwrite(false)
+        if (!downloadTarget.exists()) {
+            download().run {
+                src(url)
+                dest(downloadTarget)
+                overwrite(false)
+            }
         }
-        copy {
-            from(zipTree(downloadTarget))
-            into(path)
+        if (!path.exists()) {
+            copy {
+                from(zipTree(downloadTarget))
+                into(path)
+            }
         }
         val smtFiles = path.walkTopDown().filter { it.extension == "smt2" }.toList()
         val testResources = testResourceDir()!!
