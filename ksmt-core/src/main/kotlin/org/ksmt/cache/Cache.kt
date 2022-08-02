4252f0ff93403a9f5bf9f5c56d86e386cf446a9a
Saloed <napster_1997@mail.ru>
2022-08-02 17:34:47 +0300

    don't use weak hash maps for caching because of unpredictable boxing of primitive arguments

--- ksmt-core/src/main/kotlin/org/ksmt/cache/Cache.kt
+++ ksmt-core/src/main/kotlin/org/ksmt/cache/Cache.kt
@@ -1,8 +1,5 @@
 package org.ksmt.cache
 
-import java.lang.ref.WeakReference
-import java.util.WeakHashMap
-
 class Cache0<T>(val builder: () -> T) {
     private var value: Any? = UNINITIALIZED
 
@@ -20,59 +17,31 @@ class Cache0<T>(val builder: () -> T) {
 }
 
 class Cache1<T, A0>(val builder: (A0) -> T) {
-    private val cache = WeakHashMap<A0, WeakReference<T>>()
-    fun create(a0: A0): T {
-        val currentNode = cache[a0]?.get()
-        if (currentNode != null) return currentNode
-        val newNode = builder(a0)
-        cache[a0] = WeakReference(newNode)
-        return newNode
-    }
+    private val cache = HashMap<A0, T>()
+    fun create(a0: A0): T = cache.getOrPut(a0) { builder(a0) }
 }
 
 class Cache2<T, A0, A1>(val builder: (A0, A1) -> T) {
-    private val cache = WeakHashMap<A0, WeakHashMap<A1, WeakReference<T>>>()
-    fun create(a0: A0, a1: A1): T {
-        val node0 = cache.getOrPut(a0) { WeakHashMap() }
-        val currentNode = node0[a1]?.get()
-        if (currentNode != null) return currentNode
-        val newNode = builder(a0, a1)
-        node0[a1] = WeakReference(newNode)
-        return newNode
-    }
+    private val cache = HashMap<Pair<A0, A1>, T>()
+    fun create(a0: A0, a1: A1): T = cache.getOrPut(Pair(a0, a1)) { builder(a0, a1) }
 }
 
 class Cache3<T, A0, A1, A2>(val builder: (A0, A1, A2) -> T) {
-    private val cache = WeakHashMap<A0, WeakHashMap<A1, WeakHashMap<A2, WeakReference<T>>>>()
-    fun create(a0: A0, a1: A1, a2: A2): T {
-        val node0 = cache.getOrPut(a0) { WeakHashMap() }
-        val node1 = node0.getOrPut(a1) { WeakHashMap() }
-        val currentNode = node1[a2]?.get()
-        if (currentNode != null) return currentNode
-        val newNode = builder(a0, a1, a2)
-        node1[a2] = WeakReference(newNode)
-        return newNode
-    }
+    private val cache = HashMap<Triple<A0, A1, A2>, T>()
+    fun create(a0: A0, a1: A1, a2: A2): T = cache.getOrPut(Triple(a0, a1, a2)) { builder(a0, a1, a2) }
 }
 
 class Cache4<T, A0, A1, A2, A3>(val builder: (A0, A1, A2, A3) -> T) {
-    private val cache = WeakHashMap<A0, WeakHashMap<A1, WeakHashMap<A2, WeakHashMap<A3, WeakReference<T>>>>>()
+    private val cache = HashMap<List<*>, T>()
+
     @Suppress("unused")
-    fun create(a0: A0, a1: A1, a2: A2, a3: A3): T {
-        val node0 = cache.getOrPut(a0) { WeakHashMap() }
-        val node1 = node0.getOrPut(a1) { WeakHashMap() }
-        val node2 = node1.getOrPut(a2) { WeakHashMap() }
-        val currentNode = node2[a3]?.get()
-        if (currentNode != null) return currentNode
-        val newNode = builder(a0, a1, a2, a3)
-        node2[a3] = WeakReference(newNode)
-        return newNode
-    }
+    fun create(a0: A0, a1: A1, a2: A2, a3: A3): T = cache.getOrPut(listOf(a0, a1, a2, a3)) { builder(a0, a1, a2, a3) }
 }
 
 fun <T> mkCache(builder: () -> T) = Cache0(builder)
 fun <T, A0> mkCache(builder: (A0) -> T) = Cache1(builder)
 fun <T, A0, A1> mkCache(builder: (A0, A1) -> T) = Cache2(builder)
 fun <T, A0, A1, A2> mkCache(builder: (A0, A1, A2) -> T) = Cache3(builder)
+
 @Suppress("unused")
 fun <T, A0, A1, A2, A3> mkCache(builder: (A0, A1, A2, A3) -> T) = Cache4(builder)
