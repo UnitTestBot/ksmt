6e5347fd73c7d587d3b412146141dc370f7ded8c
Saloed <napster_1997@mail.ru>
2022-08-05 20:32:43 +0300

    don't use z3 expr translate in tests to avoid issues with uninterpreted sorts translation

--- ksmt-z3/src/test/kotlin/com/microsoft/z3/TranslateModel.kt
+++
@@ -1,6 +0,0 @@
-package com.microsoft.z3
-
-fun Model.translate(target: Context): Model {
-    val translated = Native.modelTranslate(context.nCtx(), nativeObject, target.nCtx())
-    return Model(target, translated)
-}
