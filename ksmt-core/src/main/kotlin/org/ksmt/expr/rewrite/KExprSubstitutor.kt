c99a011a544cc1fd530734d00aa7b07fd20d7513
Saloed <napster_1997@mail.ru>
2022-08-16 19:58:34 +0300

    fix non-recursive transformer

--- ksmt-core/src/main/kotlin/org/ksmt/expr/rewrite/KExprSubstitutor.kt
+++ ksmt-core/src/main/kotlin/org/ksmt/expr/rewrite/KExprSubstitutor.kt
@@ -2,12 +2,12 @@ package org.ksmt.expr.rewrite
 
 import org.ksmt.KContext
 import org.ksmt.expr.KExpr
-import org.ksmt.expr.transformer.KNonRecursiveAppTransformer
+import org.ksmt.expr.transformer.KNonRecursiveTransformer
 import org.ksmt.sort.KSort
 
 /* Substitute every occurrence of `from` in expression `expr` with `to`.
 * */
-class KExprSubstitutor(ctx: KContext) : KNonRecursiveAppTransformer(ctx) {
+class KExprSubstitutor(ctx: KContext) : KNonRecursiveTransformer(ctx) {
     private val substitution = hashMapOf<KExpr<*>, KExpr<*>>()
     fun <T : KSort> substitute(from: KExpr<T>, to: KExpr<T>) {
         substitution[from] = to
