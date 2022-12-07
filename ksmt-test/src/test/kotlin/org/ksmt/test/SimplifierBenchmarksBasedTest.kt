554f8d1f120e83689f2bf45c3c5f2b3431b62f81
Saloed <napster_1997@mail.ru>
2022-12-07 20:06:41 +0300

    Check KContext consistency after simplification

--- ksmt-test/src/test/kotlin/org/ksmt/test/SimplifierBenchmarksBasedTest.kt
+++ ksmt-test/src/test/kotlin/org/ksmt/test/SimplifierBenchmarksBasedTest.kt
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import org.ksmt.KContext
+import org.ksmt.expr.KApp
+import org.ksmt.expr.KExpr
 import org.ksmt.expr.rewrite.simplify.KExprSimplifier
+import org.ksmt.expr.transformer.KNonRecursiveTransformer
+import org.ksmt.sort.KSort
 import java.nio.file.Path
 
 class SimplifierBenchmarksBasedTest : BenchmarksBasedTest() {
@@ -15,11 +20,22 @@ class SimplifierBenchmarksBasedTest : BenchmarksBasedTest() {
     fun testSimplifier(name: String, samplePath: Path) =
         testConverter(name, samplePath) { assertions ->
             val simplifier = KExprSimplifier(ctx)
-            assertions.map { simplifier.apply(it) }
+            val simplified = assertions.map { simplifier.apply(it) }
+            simplified.forEach { ContextConsistencyChecker(ctx).apply(it) }
+            simplified
         }
 
     companion object {
         @JvmStatic
         fun simplifierTestData() = testData()
+    }
+
+    class ContextConsistencyChecker(ctx: KContext) : KNonRecursiveTransformer(ctx) {
+        override fun <T : KSort> transformApp(expr: KApp<T, *>): KExpr<T> = with(ctx) {
+            check(expr === expr.decl.apply(expr.args)) {
+                "Context is inconsistent"
+            }
+            return super.transformApp(expr)
+        }
     }
 }
