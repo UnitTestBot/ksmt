892c6a50341772a47152ef5ca240213e4c1d437d
Saloed <napster_1997@mail.ru>
2023-01-17 11:56:55 +0300

    Slightly better cache

--- ksmt-core/src/main/kotlin/org/ksmt/cache/Cache.kt
+++ ksmt-core/src/main/kotlin/org/ksmt/cache/Cache.kt
@@ -24,7 +24,7 @@ class Cache0<T>(val builder: () -> T) : AutoCloseable {
 class Cache1<T, A0>(val builder: (A0) -> T) : AutoCloseable {
     private val cache = HashMap<A0, T>()
 
-    fun create(a0: A0): T = cache.getOrPut(a0) { builder(a0) }
+    fun create(a0: A0): T = cache.computeIfAbsent(a0, builder)
 
     operator fun contains(key: A0): Boolean = key in cache
 
@@ -35,8 +35,9 @@ class Cache1<T, A0>(val builder: (A0) -> T) : AutoCloseable {
 
 class Cache2<T, A0, A1>(val builder: (A0, A1) -> T) : AutoCloseable {
     private val cache = HashMap<Pair<A0, A1>, T>()
+    private val valueBuilder = { key: Pair<A0, A1> -> builder(key.first, key.second) }
 
-    fun create(a0: A0, a1: A1): T = cache.getOrPut(Pair(a0, a1)) { builder(a0, a1) }
+    fun create(a0: A0, a1: A1): T = cache.computeIfAbsent(Pair(a0, a1), valueBuilder)
 
     override fun close() {
         cache.clear()
@@ -45,34 +46,51 @@ class Cache2<T, A0, A1>(val builder: (A0, A1) -> T) : AutoCloseable {
 
 class Cache3<T, A0, A1, A2>(val builder: (A0, A1, A2) -> T) : AutoCloseable {
     private val cache = HashMap<Triple<A0, A1, A2>, T>()
+    private val valueBuilder = { key: Triple<A0, A1, A2> -> builder(key.first, key.second, key.third) }
 
-    fun create(a0: A0, a1: A1, a2: A2): T = cache.getOrPut(Triple(a0, a1, a2)) { builder(a0, a1, a2) }
+    fun create(a0: A0, a1: A1, a2: A2): T = cache.computeIfAbsent(Triple(a0, a1, a2), valueBuilder)
 
     override fun close() {
         cache.clear()
     }
 }
+
+private data class Tuple4<A0, A1, A2, A3>(
+    val first: A0,
+    val second: A1,
+    val third: A2,
+    val fourth: A3
+)
 
 class Cache4<T, A0, A1, A2, A3>(val builder: (A0, A1, A2, A3) -> T) : AutoCloseable {
-    private val cache = HashMap<List<*>, T>()
+    private val cache = HashMap<Tuple4<A0, A1, A2, A3>, T>()
+    private val valueBuilder = { key: Tuple4<A0, A1, A2, A3> ->
+        builder(key.first, key.second, key.third, key.fourth)
+    }
 
-    @Suppress("unused")
-    fun create(a0: A0, a1: A1, a2: A2, a3: A3): T = cache.getOrPut(listOf(a0, a1, a2, a3)) { builder(a0, a1, a2, a3) }
+    fun create(a0: A0, a1: A1, a2: A2, a3: A3): T = cache.computeIfAbsent(Tuple4(a0, a1, a2, a3), valueBuilder)
 
     override fun close() {
         cache.clear()
     }
 }
 
+private data class Tuple5<A0, A1, A2, A3, A4>(
+    val first: A0,
+    val second: A1,
+    val third: A2,
+    val fourth: A3,
+    val fifth: A4
+)
+
 class Cache5<T, A0, A1, A2, A3, A4>(val builder: (A0, A1, A2, A3, A4) -> T) : AutoCloseable {
-    private val cache = HashMap<List<*>, T>()
-
-    @Suppress("unused")
-    fun create(a0: A0, a1: A1, a2: A2, a3: A3, a4: A4): T {
-        val arguments = listOf(a0, a1, a2, a3, a4)
-
-        return cache.getOrPut(arguments) { builder(a0, a1, a2, a3, a4) }
+    private val cache = HashMap<Tuple5<A0, A1, A2, A3, A4>, T>()
+    private val valueBuilder = { key: Tuple5<A0, A1, A2, A3, A4> ->
+        builder(key.first, key.second, key.third, key.fourth, key.fifth)
     }
+
+    fun create(a0: A0, a1: A1, a2: A2, a3: A3, a4: A4): T =
+        cache.computeIfAbsent(Tuple5(a0, a1, a2, a3, a4), valueBuilder)
 
     override fun close() {
         cache.clear()
@@ -83,7 +101,5 @@ fun <T> mkCache(builder: () -> T) = Cache0(builder)
 fun <T, A0> mkCache(builder: (A0) -> T) = Cache1(builder)
 fun <T, A0, A1> mkCache(builder: (A0, A1) -> T) = Cache2(builder)
 fun <T, A0, A1, A2> mkCache(builder: (A0, A1, A2) -> T) = Cache3(builder)
-
-@Suppress("unused")
 fun <T, A0, A1, A2, A3> mkCache(builder: (A0, A1, A2, A3) -> T) = Cache4(builder)
 fun <T, A0, A1, A2, A3, A4> mkCache(builder: (A0, A1, A2, A3, A4) -> T) = Cache5(builder)
