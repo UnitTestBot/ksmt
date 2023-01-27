2f688297b14aff45de50c2171499367ac53bc2f2
dmitriy sokolov <codemitry@gmail.com>
2023-01-28 00:48:25 +0300

    declarations collector fix for UF args

--- ksmt-cvc5/src/main/kotlin/io/github/cvc5/Cvc5DeclsCollector.kt
+++ ksmt-cvc5/src/main/kotlin/io/github/cvc5/Cvc5DeclsCollector.kt
@@ -6,7 +6,12 @@ class Cvc5DeclsCollector(private val terms: Collection<Term>) {
     fun Term.collect(): Set<Term> = TreeSet<Term>().apply {
         when (kind) {
             Kind.CONSTANT -> add(this@collect)
-            Kind.APPLY_UF -> add(this@collect.getChild(0)) // 0th term is function declaration in cvc5
+            Kind.APPLY_UF -> {
+                add(this@collect.getChild(0)) // 0th term is function declaration in cvc5
+                for (i in 1 until this@collect.numChildren) {
+                    addAll(this@collect.getChild(i).collect())
+                }
+            }
             else -> this@collect.asIterable().forEach { addAll(it.collect()) }
         }
     }
