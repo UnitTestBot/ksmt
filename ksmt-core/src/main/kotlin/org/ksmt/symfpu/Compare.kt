93ef123a3d16cf2cf3ca8408e79f180d2d023aa4
Mark Vavilov <vavilovmark@gmail.com>
2023-01-30 15:23:22 +0400

    change KEqExpr->KFpEqualExpr

--- ksmt-core/src/main/kotlin/org/ksmt/symfpu/Compare.kt
+++ ksmt-core/src/main/kotlin/org/ksmt/symfpu/Compare.kt
@@ -2,14 +2,14 @@ package org.ksmt.symfpu
 
 import org.ksmt.KContext
 import org.ksmt.expr.KAndExpr
-import org.ksmt.expr.KEqExpr
+import org.ksmt.expr.KFpEqualExpr
 import org.ksmt.sort.KFpSort
 
 class Compare {
     companion object {
-        fun <F : KFpSort> KContext.fpToBvExpr(expr: KEqExpr<F>): KAndExpr {
-            val left = expr.lhs
-            val right = expr.rhs
+        fun <F : KFpSort> KContext.fpToBvExpr(expr: KFpEqualExpr<F>): KAndExpr {
+            val left = expr.arg0
+            val right = expr.arg1
 
             // All comparison with NaN are false
             val neitherNan = mkFpIsNaNExpr(left).not() and mkFpIsNaNExpr(right).not()
--- ksmt-core/src/test/kotlin/org/ksmt/CompareTest.kt
+++ ksmt-core/src/test/kotlin/org/ksmt/CompareTest.kt
@@ -15,20 +15,20 @@ class CompareTest {
             interpretations = mapOf(),
             uninterpretedSortsUniverses = emptyMap()
         )
-        assertEquals(trueExpr, model.eval(fpToBvExpr(a eq mkFp32(1.6f))))
-        assertEquals(trueExpr, model.eval(fpToBvExpr(a eq a)))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(a eq mkFp32(1.7f))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(a eq mkFpNan(a.sort))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(a eq mkFpNan(a.sort))))
+        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFp32(1.6f)))))
+        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, a))))
+        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFp32(1.7f)))))
+        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFpNan(a.sort)))))
+        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFpNan(a.sort)))))
 
         val positiveInf = mkFpInf(signBit = false, mkFp32Sort())
         val negativeInf = mkFpInf(signBit = true, mkFp32Sort())
-        assertEquals(trueExpr, model.eval(fpToBvExpr(positiveInf eq positiveInf)))
-        assertEquals(trueExpr, model.eval(fpToBvExpr(negativeInf eq negativeInf)))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(positiveInf eq negativeInf)))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(positiveInf eq a)))
+        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, positiveInf))))
+        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(negativeInf, negativeInf))))
+        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, negativeInf))))
+        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, a))))
 
         val x by mkFp32Sort()
-        assertEquals(mkFpIsNaNExpr(x).not(), model.eval(fpToBvExpr(x eq x)))
+        assertEquals(mkFpIsNaNExpr(x).not(), model.eval(fpToBvExpr(mkFpEqualExpr(x, x))))
     }
 }
