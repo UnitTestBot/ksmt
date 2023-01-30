04e778c02bab1ecb5f5ce92c5f6d52bb005222d5
Mark Vavilov <vavilovmark@gmail.com>
2023-01-30 17:36:29 +0400

    test using z3 solver

--- ksmt-symfpu/src/main/kotlin/org/ksmt/symfpu/Compare.kt
+++ ksmt-symfpu/src/main/kotlin/org/ksmt/symfpu/Compare.kt
@@ -1,13 +1,14 @@
 package org.ksmt.symfpu
 
 import org.ksmt.KContext
-import org.ksmt.expr.KAndExpr
+import org.ksmt.expr.KExpr
 import org.ksmt.expr.KFpEqualExpr
+import org.ksmt.sort.KBoolSort
 import org.ksmt.sort.KFpSort
 
 class Compare {
     companion object {
-        fun <F : KFpSort> KContext.fpToBvExpr(expr: KFpEqualExpr<F>): KAndExpr {
+        fun <F : KFpSort> KContext.fpToBvExpr(expr: KFpEqualExpr<F>): KExpr<KBoolSort> {
             val left = expr.arg0
             val right = expr.arg1
 
--- ksmt-symfpu/src/test/java/org/ksmt/symfpu/CompareTest.kt
+++ ksmt-symfpu/src/test/java/org/ksmt/symfpu/CompareTest.kt
@@ -2,34 +2,25 @@ package org.ksmt.symfpu
 
 import org.junit.jupiter.api.Test
 import org.ksmt.KContext
-import org.ksmt.solver.model.KModelImpl
+import org.ksmt.solver.KSolverStatus
+import org.ksmt.solver.z3.KZ3Solver
 import org.ksmt.symfpu.Compare.Companion.fpToBvExpr
 import org.ksmt.utils.getValue
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
 
 class CompareTest {
     @Test
-    fun testDeepExpressionSort(): Unit = with(KContext()) {
-        val a = mkFp32(1.6f)
-        val model = KModelImpl(
-            this,
-            interpretations = mapOf(),
-            uninterpretedSortsUniverses = emptyMap()
-        )
-        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFp32(1.6f)))))
-        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, a))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFp32(1.7f)))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFpNan(a.sort)))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(a, mkFpNan(a.sort)))))
+    fun testFpToBvEqExpr(): Unit = with(KContext()) {
+        KZ3Solver(this).use { solver ->
+            val a by mkFp32Sort()
+            val b by mkFp32Sort()
 
-        val positiveInf = mkFpInf(signBit = false, mkFp32Sort())
-        val negativeInf = mkFpInf(signBit = true, mkFp32Sort())
-        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, positiveInf))))
-        assertEquals(trueExpr, model.eval(fpToBvExpr(mkFpEqualExpr(negativeInf, negativeInf))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, negativeInf))))
-        assertEquals(falseExpr, model.eval(fpToBvExpr(mkFpEqualExpr(positiveInf, a))))
+            solver.assert(fpToBvExpr(mkFpEqualExpr(a, b)) neq mkFpEqualExpr(a, b))
 
-        val x by mkFp32Sort()
-        assertEquals(mkFpIsNaNExpr(x).not(), model.eval(fpToBvExpr(mkFpEqualExpr(x, x))))
+            // check assertions satisfiability with timeout
+            val status = solver.check(timeout = 3.seconds)
+            assertEquals(KSolverStatus.UNSAT, status)
+        }
     }
 }
