308cf7f0eb1d924ebb228b11e6a204757ad4f59a
Mark Vavilov <vavilovmark@gmail.com>
2023-02-02 13:35:18 +0400

    eliminate fp operations

--- ksmt-symfpu/src/main/kotlin/org/ksmt/symfpu/UnpackedFp.kt
+++ ksmt-symfpu/src/main/kotlin/org/ksmt/symfpu/UnpackedFp.kt
@@ -2,13 +2,13 @@ package org.ksmt.symfpu
 
 import org.ksmt.KContext
 import org.ksmt.expr.KExpr
+import org.ksmt.expr.KOrExpr
 import org.ksmt.expr.printer.ExpressionPrinter
 import org.ksmt.expr.transformer.KTransformerBase
 import org.ksmt.sort.KFpSort
 
 class UnpackedFp<Fp : KFpSort>(
-    private val fp: KExpr<Fp>,
-    ctx: KContext
+    private val fp: KExpr<Fp>, ctx: KContext
 ) : KExpr<Fp>(ctx) {
     override val sort: Fp = fp.sort
 
@@ -24,28 +24,50 @@ class UnpackedFp<Fp : KFpSort>(
 
 
     private val exponentSize = sort.exponentBits.toInt()
-    private val significandSize = sort.significandBits.toInt()
-
     val bv = ctx.mkFpToIEEEBvExpr(fp)
-    val shift = sort.exponentShiftSize()
 
     private val size = bv.sort.sizeBits.toInt()
 
-    val sign = ctx.mkBvExtractExpr(size - 1, size - 1, bv)
-    val exponent = ctx.mkBvExtractExpr(size - 2, size - exponentSize - 1, bv)
-    val significand = ctx.mkBvExtractExpr(size - exponentSize - 2, 0, bv)
+    val sign by lazy { ctx.mkBvExtractExpr(size - 1, size - 1, bv) }
+    val exponent by lazy { ctx.mkBvExtractExpr(size - 2, size - exponentSize - 1, bv) }
+    val significand by lazy { ctx.mkBvExtractExpr(size - exponentSize - 2, 0, bv) }
 
-
-    val isNaN by lazy { ctx.mkFpIsNaNExpr(fp) }
-    val isInfinite by lazy { ctx.mkFpIsInfiniteExpr(fp) }
-    val isZero by lazy { ctx.mkFpIsZeroExpr(fp) }
-    val isNormal by lazy { ctx.mkFpIsNormalExpr(fp) }
-    val isSubnormal by lazy { ctx.mkFpIsSubnormalExpr(fp) }
-    val isNegative by lazy { ctx.mkFpIsNegativeExpr(fp) }
-
-
-    val isNegativeInfinity get() = with(ctx) { isInfinite and isNegative }
-    val isPositiveInfinity get() = with(ctx) { isInfinite and isNegative.not() }
+    val isNaN by lazy {
+        with(ctx) {
+            val nanBv = mkFpToIEEEBvExpr(mkFpNan(sort))
+            nanBv eq bv
+        }
+    }
+    val isZero by lazy {
+        with(ctx) {
+            val zeroBv = mkFpToIEEEBvExpr(mkFpZero(false, sort))
+            val negativeZeroBv = mkFpToIEEEBvExpr(mkFpZero(true, sort))
+            zeroBv eq bv or (negativeZeroBv eq bv)
+        }
+    }
+    val isNegative by lazy {
+        with(ctx) {
+            sign eq mkBv(1, 1u)
+        }
+    }
+    val isNegativeInfinity by lazy {
+        with(ctx) {
+            val infBv = mkFpToIEEEBvExpr(mkFpInf(true, sort)) // negative
+            infBv eq bv
+        }
+    }
+    val isPositiveInfinity by lazy {
+        with(ctx) {
+            val infBv = mkFpToIEEEBvExpr(mkFpInf(false, sort)) // positive
+            infBv eq bv
+        }
+    }
+    val isInfinite: KOrExpr
+        get() {
+            return with(ctx) {
+                isNegativeInfinity or isPositiveInfinity
+            }
+        }
 
     companion object {
         fun <Fp : KFpSort> KContext.unpackedFp(fp: KExpr<Fp>) = UnpackedFp(fp, this)
