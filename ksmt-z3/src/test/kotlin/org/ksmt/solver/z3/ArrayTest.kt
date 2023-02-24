c373ebd9283e505219e4eecaa26dc780797723a2
saloed <napster_1997@mail.ru>
2023-02-24 13:30:15 +0300

    Array tests

--- ksmt-z3/src/test/kotlin/org/ksmt/solver/z3/ArrayTest.kt
+++ ksmt-z3/src/test/kotlin/org/ksmt/solver/z3/ArrayTest.kt
@@ -2,9 +2,11 @@ package org.ksmt.solver.z3
 
 import com.microsoft.z3.Context
 import org.ksmt.KContext
+import org.ksmt.expr.KExpr
 import org.ksmt.sort.KSort
 import org.ksmt.utils.getValue
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ArrayTest {
 
@@ -15,11 +17,92 @@ class ArrayTest {
         val array by sort
         val index by bv32Sort
         val value by bv64Sort
+        val decl = mkFuncDecl("F", bv64Sort, listOf(bv32Sort))
 
-        val const = mkArrayConst(sort, value)
-        val select = mkArraySelect(array, index)
-        val store = mkArrayStore(array, index, value)
-        val lambda = mkArrayLambda(index.decl, select)
+        testArrayConvert(
+            mkConst = { mkArrayConst(sort, value) },
+            mkSelect = { mkArraySelect(array, index) },
+            mkStore = { mkArrayStore(array, index, value) },
+            mkLambda = { mkArrayLambda(index.decl, mkArraySelect(array, index)) },
+            mkAsArray = { mkFunctionAsArray(sort, decl) }
+        )
+    }
+
+    @Test
+    fun testArray2Conversion(): Unit = with(KContext()) {
+        val sort = mkArray2Sort(bv32Sort, bv8Sort, bv64Sort)
+
+        val array by sort
+        val index0 by bv32Sort
+        val index1 by bv8Sort
+        val value by bv64Sort
+        val decl = mkFuncDecl("F", bv64Sort, listOf(bv32Sort, bv8Sort))
+
+        testArrayConvert(
+            mkConst = { mkArrayConst(sort, value) },
+            mkSelect = { mkArraySelect(array, index0, index1) },
+            mkStore = { mkArrayStore(array, index0, index1, value) },
+            mkLambda = { mkArrayLambda(index0.decl, index1.decl, mkArraySelect(array, index0, index1)) },
+            mkAsArray = { mkFunctionAsArray(sort, decl) }
+        )
+    }
+
+    @Test
+    fun testArray3Conversion(): Unit = with(KContext()) {
+        val sort = mkArray3Sort(bv32Sort, bv8Sort, bv16Sort, bv64Sort)
+
+        val array by sort
+        val index0 by bv32Sort
+        val index1 by bv8Sort
+        val index2 by bv16Sort
+        val value by bv64Sort
+        val decl = mkFuncDecl("F", bv64Sort, listOf(bv32Sort, bv8Sort, bv16Sort))
+
+        testArrayConvert(
+            mkConst = { mkArrayConst(sort, value) },
+            mkSelect = { mkArraySelect(array, index0, index1, index2) },
+            mkStore = { mkArrayStore(array, index0, index1, index2, value) },
+            mkLambda = {
+                mkArrayLambda(
+                    index0.decl, index1.decl, index2.decl,
+                    mkArraySelect(array, index0, index1, index2)
+                )
+            },
+            mkAsArray = { mkFunctionAsArray(sort, decl) }
+        )
+    }
+
+    @Test
+    fun testArrayNConversion(): Unit = with(KContext()) {
+        val domain = listOf(bv32Sort, bv8Sort, bv32Sort, bv8Sort, bv32Sort)
+        val sort = mkArrayNSort(domain, bv64Sort)
+
+        val array by sort
+        val indices = domain.mapIndexed { i, s -> mkConst("x$i", s) }
+        val value by bv64Sort
+        val decl = mkFuncDecl("F", bv64Sort, domain)
+
+        testArrayConvert(
+            mkConst = { mkArrayConst(sort, value) },
+            mkSelect = { mkArraySelect(array, indices) },
+            mkStore = { mkArrayStore(array, indices, value) },
+            mkLambda = { mkArrayLambda(indices.map { it.decl }, mkArraySelect(array, indices)) },
+            mkAsArray = { mkFunctionAsArray(sort, decl) }
+        )
+    }
+
+    private inline fun KContext.testArrayConvert(
+        mkConst: KContext.() -> KExpr<*>,
+        mkSelect: KContext.() -> KExpr<*>,
+        mkStore: KContext.() -> KExpr<*>,
+        mkLambda: KContext.() -> KExpr<*>,
+        mkAsArray: KContext.() -> KExpr<*>
+    ) {
+        val const = mkConst()
+        val select = mkSelect()
+        val store = mkStore()
+        val lambda = mkLambda()
+        val asArray = mkAsArray()
 
         val ctx = this
         Context().use { z3Native ->
@@ -27,16 +110,24 @@ class ArrayTest {
             val z3ConvertCtx = KZ3Context(z3Native)
 
             with(KZ3ExprInternalizer(ctx, z3InternCtx)) {
-                val iConst = const.internalizeExprWrapped()
-                val iSelect = select.internalizeExprWrapped()
-                val iStore = store.internalizeExprWrapped()
-                val iLambda = lambda.internalizeExprWrapped()
+                val iConst = const.internalizeExprWrapped().simplify()
+                val iSelect = select.internalizeExprWrapped().simplify()
+                val iStore = store.internalizeExprWrapped().simplify()
+                val iLambda = lambda.internalizeExprWrapped().simplify()
+                val iAsArray = asArray.internalizeExprWrapped().simplify()
 
                 with(KZ3ExprConverter(ctx, z3ConvertCtx)) {
-                    iConst.convertExprWrapped<KSort>()
-                    iSelect.convertExprWrapped<KSort>()
-                    iStore.convertExprWrapped<KSort>()
-                    iLambda.convertExprWrapped<KSort>()
+                    val cConst = iConst.convertExprWrapped<KSort>()
+                    val cSelect = iSelect.convertExprWrapped<KSort>()
+                    val cStore = iStore.convertExprWrapped<KSort>()
+                    val cLambda = iLambda.convertExprWrapped<KSort>()
+                    val cAsArray = iAsArray.convertExprWrapped<KSort>()
+
+                    assertEquals(const, cConst)
+                    assertEquals(select, cSelect)
+                    assertEquals(store, cStore)
+                    assertEquals(lambda, cLambda)
+                    assertEquals(asArray, cAsArray)
                 }
             }
         }
