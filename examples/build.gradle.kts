f81b47975813ef308ccf679e82c084028abc042e
Saloed <napster_1997@mail.ru>
2023-03-14 17:32:54 +0300

    Upgrade version to 0.4.3

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -10,9 +10,9 @@ repositories {
 
 dependencies {
     // core
-    implementation("com.github.UnitTestBot.ksmt:ksmt-core:0.4.2")
+    implementation("com.github.UnitTestBot.ksmt:ksmt-core:0.4.3")
     // z3 solver
-    implementation("com.github.UnitTestBot.ksmt:ksmt-z3:0.4.2")
+    implementation("com.github.UnitTestBot.ksmt:ksmt-z3:0.4.3")
 }
 
 java {
