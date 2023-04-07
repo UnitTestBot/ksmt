120d411999624657e2e80e3d68ddd9c52dfc1599
Saloed <napster_1997@mail.ru>
2023-04-07 17:57:56 +0300

    Add patch description

--- ksmt-yices/bindings-native/build.sh
+++ ksmt-yices/bindings-native/build.sh
@@ -22,10 +22,24 @@ LD_FLAGS="-L $YICES_DIST/lib"
 YICES_2_JAVA_LIB_NAME="libyices2java.$LIB_EXTENSION"
 
 rm -rf yices2_java_bindings
+
+# https://github.com/SRI-CSL/yices2_java_bindings/commit/d9858e540425072443830d2638db5ffdc8c92cd1
 git clone https://github.com/SRI-CSL/yices2_java_bindings
 cd yices2_java_bindings
 git checkout d9858e5
 
+#
+# Patch description
+#
+# Bindings native part:
+# 1. Fix cardinality check in `expand function`.
+# 2. Provide methods to extract values of product/sum components
+#
+# Bindings java part:
+# 1. Add the corresponding methods and classes to interact
+# with native product/sum value extraction methods
+# 2. Disable default native library load
+#
 git apply ../yices-bindings-patch.patch
 
 rm -rf build
