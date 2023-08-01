09ef95a45e7f61e1202385896e83de4de7f7ecf9
Stephen Ostapenko <stepanos2002@gmail.com>
2023-08-01 12:55:42 +0300

    add checkpointing

--- ksmt-neurosmt/src/main/python/main.py
+++ ksmt-neurosmt/src/main/python/main.py
@@ -3,6 +3,7 @@
 import sys
 import os; os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"; os.environ["CUDA_VISIBLE_DEVICES"] = os.environ["GPU"]
 import time
+from argparse import ArgumentParser
 
 import numpy as np
 from tqdm import tqdm, trange
@@ -21,11 +22,28 @@ from Model import Model
 from LightningModel import LightningModel
 
 
+def get_args():
+    parser = ArgumentParser(description="main training script")
+    parser.add_argument("--ds", required=True)
+    parser.add_argument("--ckpt", required=False)
+
+    args = parser.parse_args()
+    print("args:")
+    for arg in vars(args):
+        print(arg, "=", getattr(args, arg))
+
+    print()
+    time.sleep(5)
+
+    return args
+
+
 if __name__ == "__main__":
     seed_everything(24, workers=True)
     torch.set_float32_matmul_precision("medium")
 
-    tr, va, te = load_data(sys.argv[1])
+    args = get_args()
+    tr, va, te = load_data(args.ds)
 
     pl_model = LightningModel()
     trainer = Trainer(
@@ -34,11 +52,15 @@ if __name__ == "__main__":
         logger=TensorBoardLogger("../logs", name="neuro-smt"),
         max_epochs=100,
         log_every_n_steps=1,
-        enable_checkpointing=False,
-        barebones=False
+        enable_checkpointing=True,
+        barebones=False,
+        default_root_dir=".."
     )
 
-    trainer.fit(pl_model, tr, va)
+    if args.ckpt is None:
+        trainer.fit(pl_model, tr, va)
+    else:
+        trainer.fit(pl_model, tr, va, ckpt_path=args.ckpt)
 
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
