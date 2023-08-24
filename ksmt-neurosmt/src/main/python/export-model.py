8b42598a9ea36d48f3b7b9ff98da6cadc1d5ced2
Stephen Ostapenko <stepanos2002@gmail.com>
2023-08-24 12:49:08 +0300

    add model export script

---
+++ ksmt-neurosmt/src/main/python/export-model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import sys
+
+import torch
+
+from LightningModel import LightningModel
+
+from Model import EMBEDDING_DIM
+
+
+if __name__ == "__main__":
+
+    pl_model = LightningModel().load_from_checkpoint(sys.argv[1], map_location=torch.device("cpu"))
+    pl_model.eval()
+
+    # input example
+    node_labels = torch.unsqueeze(torch.arange(0, 9).to(torch.int32), 1)
+    node_features = pl_model.model.encoder.embedding(node_labels.squeeze())
+    edges = torch.tensor([
+        [0, 0, 0, 1, 2, 3, 4, 4, 5, 6, 7],
+        [1, 2, 3, 3, 7, 4, 5, 6, 8, 8, 8]
+    ], dtype=torch.int64)
+
+    torch.onnx.export(
+        pl_model.model.encoder.embedding,
+        (node_labels,),
+        sys.argv[2],  # path to model file
+        opset_version=18,
+        input_names=["node_labels"],
+        output_names=["out"],
+        dynamic_axes={
+            "node_labels": {0: "nodes_number"}
+        }
+    )
+
+    torch.onnx.export(
+        pl_model.model.encoder.conv,
+        (node_features, edges),
+        sys.argv[3],  # path to model file
+        opset_version=18,
+        input_names=["node_features", "edges"],
+        output_names=["out"],
+        dynamic_axes={
+            "node_features": {0: "nodes_number"},
+            "edges": {1: "edges_number"}
+        }
+    )
+
+    torch.onnx.export(
+        pl_model.model.decoder,
+        (torch.rand((1, EMBEDDING_DIM)),),
+        sys.argv[4],  # path to model file
+        opset_version=18,
+        input_names=["expr_features"],
+        output_names=["out"],
+        dynamic_axes={
+            "expr_features": {0: "batch_size"}
+        }
+    )
+
+    print("success!")
