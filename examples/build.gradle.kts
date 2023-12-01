6491393daa709a1de42405530be3c6dd625c8fc4
Valentyn Sobol <napster_1997@mail.ru>
2023-12-01 10:56:28 +0300

    Upgrade version to 0.5.15

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -9,11 +9,11 @@ repositories {
 
 dependencies {
     // core
-    implementation("io.ksmt:ksmt-core:0.5.14")
+    implementation("io.ksmt:ksmt-core:0.5.15")
     // z3 solver
-    implementation("io.ksmt:ksmt-z3:0.5.14")
+    implementation("io.ksmt:ksmt-z3:0.5.15")
     // Runner and portfolio solver
-    implementation("io.ksmt:ksmt-runner:0.5.14")
+    implementation("io.ksmt:ksmt-runner:0.5.15")
 }
 
 java {
