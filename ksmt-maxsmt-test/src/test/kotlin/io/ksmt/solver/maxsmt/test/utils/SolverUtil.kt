ed462d34168274e141afb0a5ebd57387fe7b54c3
Viktoriia Fomina <viky.fomina@yandex.ru>
2024-05-20 16:09:29 +0300

    Updare solver enum for Z3_Native support

--- ksmt-maxsmt-test/src/test/kotlin/io/ksmt/solver/maxsmt/test/utils/SolverUtil.kt
+++ ksmt-maxsmt-test/src/test/kotlin/io/ksmt/solver/maxsmt/test/utils/SolverUtil.kt
@@ -1,7 +1,7 @@
 package io.ksmt.solver.maxsmt.test.utils
 
 enum class Solver {
-    Z3, BITWUZLA, CVC5, YICES, PORTFOLIO
+    Z3, BITWUZLA, CVC5, YICES, PORTFOLIO, Z3_NATIVE
 }
 
 enum class MaxSmtSolver {
