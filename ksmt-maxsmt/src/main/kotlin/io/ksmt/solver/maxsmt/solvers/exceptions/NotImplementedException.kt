1c1f60ac07b3a934292d9ed117bd55b5e13ff883
Viktoriia Fomina <viky.fomina@yandex.ru>
2024-06-14 22:25:32 +0300

    Remove useless NotImplementedException

--- ksmt-maxsmt/src/main/kotlin/io/ksmt/solver/maxsmt/solvers/exceptions/NotImplementedException.kt
+++
@@ -1,6 +0,0 @@
-package io.ksmt.solver.maxsmt.solvers.exceptions
-
-internal class NotYetImplementedException : RuntimeException {
-    constructor() : super()
-    constructor(message: String) : super(message)
-}
