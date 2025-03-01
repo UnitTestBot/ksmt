4c06853f5f7e51f6eb95b0a5ae375734281afee9
raf-nr <nurmuhametovrafik04@gmail.com>
2025-03-01 20:21:27 +0300

    Add helper function for eval string literal operations

--- ksmt-core/src/main/kotlin/io/ksmt/expr/rewrite/simplify/StringSimplificationRules.kt
+++ ksmt-core/src/main/kotlin/io/ksmt/expr/rewrite/simplify/StringSimplificationRules.kt
@@ -1,2 +1,16 @@
 package io.ksmt.expr.rewrite.simplify
 
+import io.ksmt.expr.KExpr
+import io.ksmt.expr.KStringLiteralExpr
+import io.ksmt.sort.KStringSort
+
+inline fun tryEvalStringLiteralOperation(
+    lhs: KExpr<KStringSort>,
+    rhs: KExpr<KStringSort>,
+    operation: (KStringLiteralExpr, KStringLiteralExpr) -> KStringLiteralExpr,
+    cont: () -> KExpr<KStringSort>
+): KExpr<KStringSort> = if (lhs is KStringLiteralExpr && rhs is KStringLiteralExpr) {
+    operation(lhs, rhs)
+} else {
+    cont()
+}
